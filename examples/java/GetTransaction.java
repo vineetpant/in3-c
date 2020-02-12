27bd1695f6aba8247edf916e8784f1c59e9660a9
Vineet Pant <vineetpant@Vineets-MacBook-Pro.local>
2020-02-12 17:16:24 +0100

    added GetBalance and GetTransaction example

--- examples/java/GetTransaction.java
+++ examples/java/GetTransaction.java
@@ -4,7 +4,8 @@ import in3.eth1.*;
 import java.math.BigInteger;
 
 public class GetTransaction {
-  //
+  
+  static String TXN_HASH = "0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e";	
   public static void main(String[] args) throws Exception {
     // create incubed
     IN3 in3 = IN3.forChain(Chain.MAINNET); // set it to mainnet (which is also dthe default)
@@ -26,7 +27,7 @@ public class GetTransaction {
 	  
 	  return in3.
 			  getEth1API().
-			  getTransactionByHash("0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e");
+			  getTransactionByHash(TXN_HASH);
 
 	  
   }
@@ -36,7 +37,7 @@ public class GetTransaction {
 	  
 	  return in3.sendRPC("eth_getTransactionByHash", 
 			  new Object[]{ 
-					  "0xdd80249a0631cf0f1593c7a9c9f9b8545e6c88ab5252287c34bc5d12457eab0e"
+					  TXN_HASH
 			  		 
 			  		 });
   }
